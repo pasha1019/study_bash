00337568a3344cc41727e1a0922b50ba5698c6da
Pavel <84281385+pasha1019@users.noreply.github.com>
2024-10-13 19:20:27 +0300

    Add script with operator 'CASE'

---
+++ students.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+var1=$1
+case $var1 in
+    0) echo "No students";;
+    1) echo "1 student";;
+    2) echo "2 students";;
+    3) echo "3 students";;
+    4) echo "4 students";;
+    *) echo "A lot of students";; 
+esac
